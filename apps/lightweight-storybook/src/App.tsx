78ec883522cf2d6f134e388537e944e45eed7c63
Yuta Takahashi <yuta.takahashi.dev@gmail.com>
2024-06-10 07:49:06 +0900

    fixup! feat: ネストしているC/Pの例を実装

--- apps/lightweight-storybook/src/App.tsx
+++ apps/lightweight-storybook/src/App.tsx
@@ -1,7 +1,7 @@
 import './styles.css';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from './lib/react-query';
-import { Button } from './components/ui/button';
+import { Button } from './components/ui/Button';
 
 function App() {
   return (
