aef59b205422581cd03f6b79fef8c2dc977c8ec1
Yuta Takahashi <yuta.takahashi.dev@gmail.com>
2024-06-13 08:53:23 +0900

    feat: アプリケーション上での動作確認

--- apps/lightweight-storybook/src/App.tsx
+++ apps/lightweight-storybook/src/App.tsx
@@ -1,18 +1,12 @@
 import './styles.css';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from './lib/react-query';
-import { Button } from './components/ui/Button';
+import { UserSettingsFormDialog } from './components/usecases/user/UserSettingsFormDialog';
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Button
-        onClick={() => {
-          alert('Hello, world!');
-        }}
-      >
-        Click me
-      </Button>
+      <UserSettingsFormDialog userId="1" />
     </QueryClientProvider>
   );
 }
